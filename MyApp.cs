5acfcc5f72b716cf47135f8ac7e3147795fefdbb
Kazuma <21kazuma21@gmail.com>
2024-03-28 22:20:46 +0900

    抽象クラスが終了

--- MyApp.cs
+++ MyApp.cs
@@ -3,30 +3,30 @@ using System.ComponentModel;
 using System.Reflection.Metadata;
 using Microsoft.VisualBasic;
 
-class User {
-  public string name;
-  public User(string name) {
-    this.name = name;
-  }
-  public User(): this("nobody"){
-  }
-  public virtual void SayHi() => Console.WriteLine($"Hi! {this.name}");
+abstract class User{
+  public abstract void SayHi();
 }
 
-class AdminUser: User {
-  public AdminUser(string name): base(name){
-  }
-  public void SayHello() => Console.WriteLine($"Hello! {this.name}");
-  public override void SayHi() => Console.WriteLine($"[admin] Hi! {this.name}");
+class Japanese: User{
+    public override void SayHi(){
+      Console.WriteLine("こんにちは");
+    }
 }
 
-class MyApp { 
-  static void Main() {
-  User tom = new User("Tom");
-  tom.SayHi();
-  AdminUser bob = new AdminUser("Bob");
-  bob.SayHi();
-  bob.SayHello();
+class American: User{
+    public override void SayHi(){
+      Console.WriteLine("Hello");
+    }
+}
+
+class MyApp{
+  static void Main(){
+    Japanese aki = new Japanese();
+    aki.SayHi();
+    American tom = new American();
+    tom.SayHi();
   }
 }
+
+
 
